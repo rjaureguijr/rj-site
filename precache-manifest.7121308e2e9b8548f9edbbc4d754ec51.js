b60b010c8adf0c7851ddbc3b0b5cab2a2fedc44f
Rudy Jauregui <rjaureguijr@gmail.com>
2020-09-02 15:58:27 -0500

    Updates

---
+++ precache-manifest.7121308e2e9b8548f9edbbc4d754ec51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "010aa1d5fc6ae84eb683b8c01032ae27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b557d5fbecc8fce59a15",
+    "url": "/static/css/main.ecf72bea.chunk.css"
+  },
+  {
+    "revision": "e0ea6ed906f28f1c162b",
+    "url": "/static/js/2.86ff97d3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.86ff97d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b557d5fbecc8fce59a15",
+    "url": "/static/js/main.7fb05345.chunk.js"
+  },
+  {
+    "revision": "ed2dc0f795ec797cd124",
+    "url": "/static/js/runtime-main.f04f0e6f.js"
+  }
+]);
